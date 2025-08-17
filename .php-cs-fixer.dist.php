9af3b60b28c13d7a395912a38085549829972f49
Felipe de Almeida Baccaro <you@example.com>
2025-08-17 14:02:58 -0300

    Initializing project

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+$finder = (new \PhpCsFixer\Finder())
+    ->in([__DIR__ . '/src', __DIR__ . '/tests']);
+
+return (new \PhpCsFixer\Config())
+    ->setRules(array(
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'phpdoc_to_comment' => false,
+    ))
+    ->setRiskyAllowed(true)
+    ->setFinder($finder);
---
+++ MenuManagerBundle.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Wiredupdev\MenuManagerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class MenuManagerBundle extends AbstractBundle
+{}
