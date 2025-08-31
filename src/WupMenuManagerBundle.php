7a6b175cbc6687aafac707ecf4699b164dabe31c
Felipe de Almeida Baccaro <wiredupdev@gmail.com>
2025-08-31 16:43:36 -0300

    Following name conventions when naming bundle.

--- src/WupMenuManagerBundle.php
+++ src/WupMenuManagerBundle.php
@@ -7,28 +7,36 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class MenuManagerBundle extends AbstractBundle
+class WupMenuManagerBundle extends AbstractBundle
 {
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
        $container->import(__DIR__ . '/../config/services.xml');
+
     }
 
 
     public function configure(DefinitionConfigurator $definition): void
     {
         $definition->rootNode()
+            ->fixXmlConfig('menu')
             ->children()
-              ->arrayNode('menu')
+              ->arrayNode('menus')
+                ->arrayPrototype()
                 ->children()
                 ->enumNode('type')
                     ->values(['class'])
                     ->isRequired()
                     ->cannotBeEmpty()
                 ->end()
+                ->scalarNode('reference')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                ->end()
               ->end()
             ->end();
     }
+
 
 
 }
