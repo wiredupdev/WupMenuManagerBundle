7a5cd79c68285ff18b0569549a29ed431a020ff4
Felipe Almeida Baccaro <wiredupdev@gmail.com>
2026-04-23 13:34:28 -0300

    Revert "adding tests for UrlResolverProcessor."
    This reverts commit 798b054b180e17b951ab932643227e2f8ef6e5b1.

--- src/Menu/Processor/UrlResolverProcessor.php
+++ src/Menu/Processor/UrlResolverProcessor.php
@@ -17,15 +17,15 @@ readonly class UrlResolverProcessor implements ProcessInterface
 
     public function process(Item $item): void
     {
-        if (false === isset($item->getOption('route')['name']) && null === $item->getUrl()) {
+        if (null === $item->getOption('route') && null === $item->getUrl()) {
             return;
         }
 
-        if (isset($item->getOption('route')['name'])) {
+        if ($item->getOption('route')) {
             $item->setUrl(
                 $this->urlGenerator->generate(
                     $item->getOption('route')['name'],
-                    $item->getOption('route')['parameters'] ?? [])
+                    $item->getOption('route')['parameters'])
             );
         }
 
